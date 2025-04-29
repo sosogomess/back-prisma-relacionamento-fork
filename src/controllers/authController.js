6a95275d0bf99da1598f966885168b1cb0d21d4a
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-04-29 11:02:47 -0300

    feat: create login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import UserModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 class AuthController {
   // Listar todos os usuários
   async getAllUsers(req, res) {
@@ -34,24 +34,74 @@ class AuthController {
       // Hash da senha
       const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Cria objeto do usuário
-        const data = {
-            name,
-            email,
-            password: hashedPassword,
-        };
+      // Cria objeto do usuário
+      const data = {
+        name,
+        email,
+        password: hashedPassword,
+      };
 
-        // Cria usuário
-        const user = await UserModel.create(data);
+      // Cria usuário
+      const user = await UserModel.create(data);
 
-        return res.status(201).json({
-          message: "Usuário criado com sucesso!",
-          user,
-        });
+      return res.status(201).json({
+        message: "Usuário criado com sucesso!",
+        user,
+      });
     } catch (error) {}
     console.error("Erro ao criar um novo usuário: ", error);
     res.status(500).json({ error: "Erro ao criar um novo usuário" });
   }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      // Validação básica
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Email e senha são obrigatórios!" });
+      }
+
+      // Verifica se o usuário existe
+      const userExists = await UserModel.findByEmail(email);
+      if (!userExists) {
+        return res.status(401).json({ error: "Credenciais inválidas!" });
+      }
+
+      // Verifica senha
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        userExists.password
+      );
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Credenciais inválidas!" });
+      }
+
+      // Gera token JWT
+      const token = jwt.sign({
+        id: userExists.id,
+        name: userExists.name,
+        email: userExists.email,
+      },
+        process.env.JWT_SECRET,
+        {
+           expiresIn: "24h",
+        }
+    );
+
+      return res.json({
+        message: "Login realizado com sucesso!",
+        token,
+        userExists,
+      });
+    } catch (error) {
+      console.error("Erro ao realizar login: ", error);
+      res.status(500).json({ error: "Erro ao realizar login" });
+    }
+  }
 }
+
 
 export default new AuthController();
