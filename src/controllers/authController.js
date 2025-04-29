1d74b53a3cb84ba9a63ce4c1631f37bb247fe146
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-04-29 08:57:31 -0300

    feat: create user controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,57 @@
+import UserModel from "../models/userModel";
+import bcrypt from "bcryptjs";
+
+class AuthController {
+  // Listar todos os usuários
+  async getAllUsers(req, res) {
+    try {
+      const users = await UserModel.findAll();
+      res.json(users);
+    } catch (error) {
+      console.error("Erro ao listar usuários:", error);
+      res.status(500).json({ error: "Erro ao listar usuários" });
+    }
+  }
+
+  // Refistrar novo usuário
+  async register(req, res) {
+    try {
+      const { name, email, password } = req.body;
+
+      //Validação básica
+      if (!name || !email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Os campos nome, email e senha sãó obrigatórios!" });
+      }
+
+      // Verifica se o usuário já existe
+      const userExists = await UserModel.findByEmail(email);
+      if (userExists) {
+        return res.status(400).json({ error: "Este email já está em uso!" });
+      }
+
+      // Hash da senha
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Cria objeto do usuário
+        const data = {
+            name,
+            email,
+            password: hashedPassword,
+        };
+
+        // Cria usuário
+        const user = await UserModel.create(data);
+
+        return res.status(201).json({
+          message: "Usuário criado com sucesso!",
+          user,
+        });
+    } catch (error) {}
+    console.error("Erro ao criar um novo usuário: ", error);
+    res.status(500).json({ error: "Erro ao criar um novo usuário" });
+  }
+}
+
+export default new AuthController();
