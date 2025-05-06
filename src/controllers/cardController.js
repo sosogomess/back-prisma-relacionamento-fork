04a375632d79f549d388fcac286034d531bf9f83
Sophia gOMES <sophiagomes.tds2@gmail.com>
2025-05-06 14:57:22 -0300

    feat: test req params

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -3,6 +3,12 @@ import CardModel from "../models/cardModel.js";
 class CardController {
   // GET /cartas
   async getAllCards(req, res) {
+    const pagina = req.query.page;
+    console.log("Página:", pagina);
+
+    const limite = req.query.limit;
+    console.log("Limite:", limite);
+    
     try {
       const cartas = await CardModel.findAll();
       res.json(cartas);
